530bdd8ffa49e0e5fb1cb501b744cc9354fbe236
Sbaoor <2959435045@qq.com>
2022-01-23 10:08:59 +0800

    fix

--- LXLEssential.js
+++ LXLEssential.js
@@ -28,7 +28,7 @@
  * update:https://raw.githubusercontent.com/LiteLDev-LXL/LXLEssential/main/LXLEssential.js
  */
 
-const version = '1.3.9.4';
+const version = '1.3.9.4fix';
 const lang_version = 1.3;
 const dir_path = './plugins/LXLEssential/';
 const lang_dir = dir_path + 'lang/';
