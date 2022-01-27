61b6dc57965a880d648d77d9e8873b0249dd452f
Lition802 <2959435045@qq.com>
2022-01-27 11:35:02 +0800

    fix

--- LXLEssential.js
+++ LXLEssential.js
@@ -23,7 +23,7 @@
  * update:https://raw.githubusercontent.com/LiteLDev-LXL/LXLEssential/main/LXLEssential.js
  */
 
-const version = '1.4.0.2';
+const version = '1.4.0.2fix';
 const lang_version = 1.7;
 const dir_path = './plugins/LXLEssential/';
 const lang_dir = dir_path + 'lang/';
